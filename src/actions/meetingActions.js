483afb51cf6676b8c87ecdba6b6aff7473a1e5b7
Jor_Poon <jormpoon@gmail.com>
2019-04-25 16:18:08 -0400

    made constants folder and actionType files, moved all action types to the file

--- src/actions/meetingActions.js
+++ src/actions/meetingActions.js
@@ -1,11 +1,23 @@
 import axios from "axios";
+import {
+    GET_MEETINGS_START,
+    GET_MEETINGS_SUCCESS,
+    GET_MEETINGS_FAILURE,
+    CREATE_MEETING_START,
+    CREATE_MEETING_SUCCESS,
+    CREATE_MEETING_FAILURE,
+    UPDATE_MEETING_START,
+    UPDATE_MEETING_SUCCESS,
+    UPDATE_MEETING_FAILURE,
+    DELETE_MEETING_START,
+    DELETE_MEETING_SUCCESS,
+    DELETE_MEETING_FAILURE
+} from "../constants/actionTypes.js"
 
 //backend  meetings url
 const url = "http://mentorbe.tfolbrecht.com/";
 
-export const GET_MEETINGS_START = "GET_MEETINGS_START";
-export const GET_MEETINGS_SUCCESS = "GET_MEETINGS_SUCCESS";
-export const GET_MEETINGS_FAILURE = "GET_MEETINGS_FAILURE";
+
 
 export const getMeetings = () => dispatch => {
   dispatch({ type: GET_MEETINGS_START });
@@ -20,9 +32,7 @@ export const getMeetings = () => dispatch => {
     });
 };
 
-export const CREATE_MEETING_START = "CREATE_MEETING_START";
-export const CREATE_MEETING_SUCCESS = "CREATE_MEETING_SUCCESS";
-export const CREATE_MEETING_FAILURE = "CREATE_MEETING_FAILURE";
+
 
 export const createMeeting = info => dispatch => {
   dispatch({ type: CREATE_MEETING_START });
@@ -38,9 +48,7 @@ export const createMeeting = info => dispatch => {
     });
 };
 
-export const UPDATE_MEETING_START = "UPDATE_MEETING_START";
-export const UPDATE_MEETING_SUCCESS = "UPDATE_MEETING_SUCCESS";
-export const UPDATE_MEETING_FAILURE = "UPDATE_MEETING_FAILURE";
+
 
 export const updateMeeting = info => dispatch => {
   dispatch({ type: UPDATE_MEETING_START });
@@ -56,9 +64,7 @@ export const updateMeeting = info => dispatch => {
     });
 };
 
-export const DELETE_MEETING_START = "DELETE_MEETING_START";
-export const DELETE_MEETING_SUCCESS = "DELETE_MEETING_SUCCESS";
-export const DELETE_MEETING_FAILURE = "DELETE_MEETING_FAILURE";
+
 
 export const deleteMeeting = id => dispatch => {
   dispatch({ type: DELETE_MEETING_START });
