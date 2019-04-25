f3628e01af2afade4fbd5f10892e2b636eaa7a7b
jdspell <spelljd15@gmail.com>
2019-04-25 16:50:21 -0400

    created get organization action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const GET_ORGANIZATION_START = "GET_ORGANIZATION_START";
 export const GET_ORGANIZATION_SUCCESS = "GET_ORGANIZATION_SUCCESS";
 export const GET_ORGANIZATION_FAILURE = "GET_ORGANIZATION_FAILURE";
@@ -14,3 +16,29 @@ export const DELETE_ORGANIZATION_START = "GET_ORGANIZATION_START";
 export const DELETE_ORGANIZATION_SUCCESS = "GET_ORGANIZATION_SUCCESS";
 export const DELETE_ORGANIZATION_FAILURE = "GET_ORGANIZATION_FAILURE";
 
+const url = 'http://mentorbe.tfolbrecht.com/';
+
+export const getOrganization = () => dispatch => {
+    dispatch({ type: GET_ORGANIZATION_START });
+    
+    return axios    
+        .get(url)
+        .then(res => {
+            dispatch({ type: GET_ORGANIZATION_SUCCESS });
+        })
+        .catch(err => {
+            dispatch({ type: GET_ORGANIZATION_FAILURE })
+        });
+};
+
+export const createOrganization = () => dispatch => {
+
+}
+
+export const updateOrganization = () => dispatch => {
+
+}
+
+export const deleteOrganization = () => dispatch => {
+
+}
