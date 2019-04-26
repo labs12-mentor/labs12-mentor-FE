b378fb491475df6f0ff798fcdb57e91fcb78cbc9
jdspell <spelljd15@gmail.com>
2019-04-26 08:49:57 -0400

    added action creator for user login to user actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import {
     CREATE_USER_START,
     CREATE_USER_SUCCESS,
-    CREATE_USER_FAILURE
+    CREATE_USER_FAILURE,
+    LOGIN_USER_START,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILURE
 } from '../constants/actionTypes';
 
 const url = 'http://mentorbe.tfolbrecht.com/';
@@ -18,4 +21,18 @@ export const registerUser = newUser => dispatch => {
         .catch(err => {
             dispatch({ type: CREATE_USER_FAILURE, payload: err });
         });
+};
+
+
+export const loginUser = credentials => {
+    dispatch({ type: LOGIN_USER_START });
+
+    return axios
+        .post(url, credentials)
+        .then(res => {
+            dispatch({ type: LOGIN_USER_SUCCESS, payload: res.data });
+        })
+        .catch(err => {
+            dispatch({ type: LOGIN_USER_FAILURE, payload: err });
+        })
 }
