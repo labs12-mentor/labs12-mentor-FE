eaf0266b6e5e9135fe0cdd8cea1556525b1fd729
jdspell <spelljd15@gmail.com>
2019-04-27 13:27:14 -0400

    created student profile component

---
+++ src/pages/StudentProfile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
+
+class StudentProfile extends React.Component {
+    render() {
+        return (
+            <div className="StudentProfile">
+
+            </div>
+        );
+    }
+}
+
+export default StudentProfile;
