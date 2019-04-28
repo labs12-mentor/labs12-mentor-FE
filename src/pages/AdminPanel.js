29cc51125b808e64fe14b56936e815cb09a776d6
jdspell <spelljd15@gmail.com>
2019-04-28 10:57:48 -0400

    setup tabs for the admin panel

--- src/pages/AdminPanel.js
+++ src/pages/AdminPanel.js
@@ -1,19 +1,77 @@
 import React from 'react';
 import {
-    Form,
-    FormGroup
+    Nav,
+    NavItem,
+    NavLink,
+    TabContent,
+    TabPane,
+    Row,
+    Col,
+    InputGroup
 } from 'reactstrap';
+import classnames from 'classnames';
 
 
 class AdminPanel extends React.Component {
     state = {
+        activeTab: '1'
+    }
 
+    toggle = tab => {
+        if(this.state.activeTab !== tab){
+            this.setState({
+                activeTab: tab
+            });
+        }
     }
 
     render() {
         return (
             <div className="AdminPanel">
-                <h1>hello</h1>
+                <h1> Administrator Panel </h1>
+
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink 
+                            className={classnames({ active: this.state.activeTab === '1' })}
+                            onClick={() => { this.toggle('1'); }} 
+                        >
+                            Mentor Applications
+                        </NavLink>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLink 
+                            className={classnames({ active: this.state.activeTab === '2' })}
+                            onClick={() => { this.toggle('2'); }} 
+                        >
+                            Mentor Assignment
+                        </NavLink>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLink 
+                            className={classnames({ active: this.state.activeTab === '3' })}
+                            onClick={() => { this.toggle('3'); }} 
+                        >
+                            Profile Forms
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+
+                <TabContent>
+                    <TabPane tabId="1">
+                        
+                    </TabPane>
+
+                    <TabPane tabId="2">
+
+                    </TabPane>
+
+                    <TabPane tabId="3">
+
+                    </TabPane>
+                </TabContent>
             </div>
         );
     }
