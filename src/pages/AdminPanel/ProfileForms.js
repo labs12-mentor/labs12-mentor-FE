f9a7656bee715bb3d00bc4b541f5d46f56d735be
jdspell <spelljd15@gmail.com>
2019-04-29 08:53:39 -0400

    added text inputs on profile forms tab in the admin panel

--- src/pages/AdminPanel/ProfileForms.js
+++ src/pages/AdminPanel/ProfileForms.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import {
-    InputGroup,
-    Input
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button
 } from 'reactstrap';
 
 class ProfileForms extends React.Component {
     render() {
         return (
             <div className="ProfileForms">
-                <InputGroup>
-                    <Input placeholder="Search by email or name" />
-                </InputGroup>
+                <Form>
+                    <FormGroup>
+                        <Label for="profileLinks">Profile Links</Label>
+                        <Input type="textarea" name="profileLinks" />
+                    </FormGroup>
+                    <Button>Add Profile Link Option</Button>
+
+                    <FormGroup>
+                        <Label for="interests">Interests</Label>
+                        <Input type="textarea" name="interests" />
+                    </FormGroup>
+                    <Button>Add Interests Option</Button>
+
+                    <FormGroup>
+                        <Label for="experience">Experience</Label>
+                        <Input type="textarea" name="experience" />
+                    </FormGroup>
+                    <Button>Add Experience Option</Button>
+
+                    <FormGroup>
+                        <Label for="openQuestionField">Open Question Field</Label>
+                        <Input type="textarea" name="openQuestionField" />
+                    </FormGroup>
+                    <Button>Add an Open Question Field</Button>
+                </Form>
             </div>
         );
     }
