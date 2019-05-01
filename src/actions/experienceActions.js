c5b60e3948c72ea8f07307494ae787aa634e5697
Jor_Poon <jormpoon@gmail.com>
2019-05-01 17:45:45 -0400

    formatted document

--- src/actions/experienceActions.js
+++ src/actions/experienceActions.js
@@ -1,68 +1,67 @@
 import axios from "axios";
 import {
-GET_EXPERIENCES_START,
-GET_EXPERIENCES_SUCCESS,
-GET_EXPERIENCES_FAILURE,
-GET_SPECIFIC_EXPERIENCE_START,
-GET_SPECIFIC_EXPERIENCE_SUCCESS,
-GET_SPECIFIC_EXPERIENCE_FAILURE,
-UPDATE_EXPERIENCE_START,
-UPDATE_EXPERIENCE_SUCCESS,
-UPDATE_EXPERIENCE_FAILURE,
-DELETE_EXPERIENCE_START,
-DELETE_EXPERIENCE_SUCCESS,
-DELETE_EXPERIENCE_FAILURE
+  GET_EXPERIENCES_START,
+  GET_EXPERIENCES_SUCCESS,
+  GET_EXPERIENCES_FAILURE,
+  GET_SPECIFIC_EXPERIENCE_START,
+  GET_SPECIFIC_EXPERIENCE_SUCCESS,
+  GET_SPECIFIC_EXPERIENCE_FAILURE,
+  UPDATE_EXPERIENCE_START,
+  UPDATE_EXPERIENCE_SUCCESS,
+  UPDATE_EXPERIENCE_FAILURE,
+  DELETE_EXPERIENCE_START,
+  DELETE_EXPERIENCE_SUCCESS,
+  DELETE_EXPERIENCE_FAILURE
 } from "../constants/actionTypes.js";
 
 //backend meetings url
 const url = "http://mentorbe.tfolbrecht.com";
 
 export const getExperiences = () => dispatch => {
-    dispatch({ type: GET_EXPERIENCES_START });
-    return axios
-      .get(`${url}/experiences`)
-      .then(res => {
-        //console.log(res);
-        dispatch({ type: GET_EXPERIENCES_SUCCESS, payload: res.data });
-      })
-      .catch(err => {
-        dispatch({ type: GET_EXPERIENCES_FAILURE, payload: err });
-      });
-  };
+  dispatch({ type: GET_EXPERIENCES_START });
+  return axios
+    .get(`${url}/experiences`)
+    .then(res => {
+      //console.log(res);
+      dispatch({ type: GET_EXPERIENCES_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: GET_EXPERIENCES_FAILURE, payload: err });
+    });
+};
 
 export const getSpecificExperience = id => dispatch => {
-    dispatch({type: GET_SPECIFIC_EXPERIENCE_START});
-    return axios
+  dispatch({ type: GET_SPECIFIC_EXPERIENCE_START });
+  return axios
     .get(`${url}/experiences/${id}`)
     .then(res => {
-        dispatch({type: GET_SPECIFIC_EXPERIENCE_SUCCESS, payload: res.data })
+      dispatch({ type: GET_SPECIFIC_EXPERIENCE_SUCCESS, payload: res.data });
     })
     .catch(err => {
-        dispatch({type: GET_SPECIFIC_EXPERIENCE_FAILURE, payload: err})
+      dispatch({ type: GET_SPECIFIC_EXPERIENCE_FAILURE, payload: err });
     });
-}
+};
 
 export const updateExperience = (id, info) => dispatch => {
-    dispatch({type: UPDATE_EXPERIENCE_START})
-    return axios
+  dispatch({ type: UPDATE_EXPERIENCE_START });
+  return axios
     .put(`{url}/experiences/${id}`, info)
     .then(res => {
-        dispatch({type: UPDATE_EXPERIENCE_SUCCESS, payload: res.data})
+      dispatch({ type: UPDATE_EXPERIENCE_SUCCESS, payload: res.data });
     })
     .catch(err => {
-        dispatch({type: UPDATE_EXPERIENCE_FAILURE, payload: err})
-    })
-}
-
+      dispatch({ type: UPDATE_EXPERIENCE_FAILURE, payload: err });
+    });
+};
 
 export const deleteExperience = id => dispatch => {
-    dispatch({type: DELETE_EXPERIENCE_START})
-    return axios
+  dispatch({ type: DELETE_EXPERIENCE_START });
+  return axios
     .delete(`${url}/experiences/${id}`)
     .then(res => {
-        dispatch({type: DELETE_EXPERIENCE_SUCCESS, payload: res.data})
+      dispatch({ type: DELETE_EXPERIENCE_SUCCESS, payload: res.data });
     })
     .catch(err => {
-        dispatch({type: DELETE_EXPERIENCE_FAILURE, payload: err})
-    })
-}
+      dispatch({ type: DELETE_EXPERIENCE_FAILURE, payload: err });
+    });
+};
