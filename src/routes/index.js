ec7e3f9cf6fb22d5c87cbd9de9559b956016d898
Jor_Poon <jormpoon@gmail.com>
2019-05-01 13:49:41 -0400

    updating changes in branch

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,8 @@ import {
 import OrganizationRegister from '../pages/OrganizationRegister';
 import userRegister from '../pages/UserRegistration';
 import userLogin from '../pages/UserLogin';
-<<<<<<< HEAD
-import MeetingsPage from '../pages/MeetingsPage.js'
-=======
 import StudentProfile from '../pages/StudentProfile';
-import AdminPanel from '../pages/AdminPanel/AdminPanel';
->>>>>>> a7203b93f014607a43c8bc4fd93f5117a25fb6ad
+import AdminPanel from '../pages/AdminPanel/AdminPanel'
 
 const Routes = (props) => {
     return (
@@ -20,12 +16,8 @@ const Routes = (props) => {
             <Route exact path="/organization/register" component={OrganizationRegister} />
             <Route exact path="/user/register" component={userRegister} />
             <Route exact path="/user/login" component={userLogin} />
-<<<<<<< HEAD
-            <Route exact path="/meetings" component={MeetingsPage}/>
-=======
             <Route exact path="/user/student/profile" component={StudentProfile} />
             <Route exact path="/user/admin/profile" component={AdminPanel} />
->>>>>>> a7203b93f014607a43c8bc4fd93f5117a25fb6ad
         </Router>
     )
 }
