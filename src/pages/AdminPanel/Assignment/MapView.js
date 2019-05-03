f6584353c1e10e86de4792ff9a2c1a9c2b5f322b
jdspell <spelljd15@gmail.com>
2019-05-03 11:12:24 -0400

    added skeleton of assignments page

---
+++ src/pages/AdminPanel/Assignment/MapView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+
+} from 'reactstrap';
+
+class MapView extends React.Component {
+    render() {
+        return (
+            <div className="MapView">
+            
+            </div>
+        );
+    }
+}
+
+export default MapView;
