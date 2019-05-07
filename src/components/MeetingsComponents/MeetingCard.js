9c5a17e263fe7ed5dea305232b67500733976431
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-06 21:09:46 -0700

    added meetings components

---
+++ src/components/MeetingsComponents/MeetingCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const MeetingCard = ({
+    id,
+    content,
+    match_id,
+    updateMeeting,
+    removeMeeting
+}) => {
+    return (
+        <div>
+            <h2>Title</h2>
+            <h3>{content}</h3>
+            <h4>Match_id:{match_id}</h4>
+
+            <button onClick={updateMeeting}>Edit</button>
+            <button onClick={removeMeeting}>Delete</button>
+        </div>
+    );
+};
+
+export default MeetingCard;
---
+++ src/components/MeetingsComponents/MeetingsList.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getMeetings } from "../../actions/meetingActions.js";
+import MeetingCard from "./MeetingCard.js";
+
+class MeetingsList extends React.Component {
+    componentDidMount() {
+        this.props.getMeetings();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Your Meetings</h1>
+
+                <h2>Upcoming Meetings</h2>
+                {this.props.meetingsList.map(meeting => {
+                    console.log(meeting);
+                    return (
+                        <MeetingCard
+                            content={meeting.content}
+                            match_id={meeting.match_id}
+                            key={meeting.id}
+                            removeMeeting={this.props.deleteMeeting}
+                            updateMeeting={this.props.updateMeeting}
+                        />
+                    );
+                })}
+
+                <h2>Past Meetings</h2>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        meetingsList: state.meetingsReducer.meetingsList
+
+        // this is to test if meetingsList is being mapped to MeetingCard component
+        // meetingsList: [{ id: 1 }, { id: 2 }, { id: 3 }]
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    { getMeetings }
+)(MeetingsList);
