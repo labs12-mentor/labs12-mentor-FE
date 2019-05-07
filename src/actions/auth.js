e30ed81eb3aea74eae8b92ca1ec76e050565e3b4
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-06 20:44:21 -0700

    fixed auth actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,6 +3,5 @@ import {
 } from '../constants/actionTypes';
 import axios from 'axios';
 import { API_URL } from '../constants/config';
-// import { API_URL_DEV as API_URL } from '../constants/config';
 import { history } from '../';
 
