4b389a02bf239ff2060aaa75c14ab890ef90f08a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-07 00:05:20 -0700

    updated organizations reducer

--- src/reducers/organizations.js
+++ src/reducers/organizations.js
@@ -25,44 +25,24 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch(action.type){
-        case GET_ORGANIZATION_START:
+        case GET_SPECIFIC_ORGANIZATION_START:
             return {
                 ...state,
                 isFetching: true
             };
 
-        case GET_ORGANIZATION_SUCCESS:
+        case GET_SPECIFIC_ORGANIZATION_SUCCESS:
             return {
                 ...state,
                 organization: action.payload,
                 isFetching: false
             };
 
-        case GET_ORGANIZATION_FAILURE:
+        case GET_SPECIFIC_ORGANIZATION_FAILURE:
             return {
                 ...state,
                 error: action.payload,
                 isFetching: false
-            };
-
-        case CREATE_ORGANIZATION_START:
-            return {
-                ...state,
-                creatingOrg: true
-            };
-
-        case CREATE_ORGANIZATION_SUCCESS:
-            return {
-                ...state,
-                organization: action.payload,
-                creatingOrg: false
-            };
-
-        case CREATE_ORGANIZATION_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                creatingOrg: false
             };
 
         case UPDATE_ORGANIZATION_START:
