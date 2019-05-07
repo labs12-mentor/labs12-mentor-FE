8e938d228ffa2ce37e9971fca85bbaa1ce1ae186
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-07 00:19:11 -0700

    fixed typo in address

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://labs12-backend-dev.netlify.com/api';
+export const API_URL = 'https://labs12-backend-dev.herokuapp.com/api';
 export const API_URL_DEV = 'http://localhost:5000/api';
