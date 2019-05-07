422fbde2093f939ea20a11e191236cc831b6f6a7
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-06 21:10:34 -0700

    added meetings container

---
+++ src/containers/MeetingsContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { connect } from "react-redux";
+import MeetingsForm from "../components/MeetingsComponents/MeetingsForm.js";
+import MeetingsList from "../components/MeetingsComponents/MeetingsList";
+import {
+    createMeeting,
+    deleteMeeting,
+    updateMeeting,
+    getSpecificMeeting
+} from "../actions/meetingActions.js";
+
+class MeetingsContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <MeetingsList
+                    deleteMeeting={this.removeMeeting}
+                    updateMeeting={this.editMeeting}
+                    getSpecificMeeting={this.getMeeting}
+                />
+                <MeetingsForm addMeeting={this.props.createMeeting} />
+            </div>
+        );
+    }
+    removeMeeting = id => {
+        //this.props.deleteMeeting(id);
+        console.log("hi");
+    };
+
+    editMeeting = (id, info) => {
+        //this.props.updateMeeting(id, info)
+        console.log("hi");
+    };
+
+    getMeeting = id => {
+        //this.props.getSpecificMeeting(id);
+    };
+
+    addMeeting = () => {};
+}
+
+export default connect(
+    null,
+    { createMeeting, deleteMeeting, updateMeeting, getSpecificMeeting }
+)(MeetingsContainer);
