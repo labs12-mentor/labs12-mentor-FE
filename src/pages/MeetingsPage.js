4dab2cad90423602723b85a174233beea83f2665
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-06 20:04:58 -0700

    project cleanup

--- src/pages/MeetingsPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MeetingsContainer from "../containers/MeetingsContainer";
-
-const MeetingsPage = props => {
-  return <MeetingsContainer />;
-};
-
-export default MeetingsPage;
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducers from '../reducers';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-
-export default createStore(
-    reducers,
-    applyMiddleware(thunk, logger)
-);
