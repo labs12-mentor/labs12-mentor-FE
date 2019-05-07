224e7b432752a343e8cfcb8d444a3ed65394cbfa
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-06 20:21:26 -0700

    added notifications reducer

---
+++ src/reducers/notifications.js
@@ -0,0 +1,14 @@
+import {
+    
+} from '../constants/actionTypes';
+
+const initialState = {
+    
+}
+
+export default (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
