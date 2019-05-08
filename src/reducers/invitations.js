4481724cc3901b88e8b60b16f983fa5be763d4dd
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-08 09:48:03 -0700

    added reducer cases for fetching invitations

--- src/reducers/invitations.js
+++ src/reducers/invitations.js
@@ -8,9 +8,6 @@ import {
     CREATE_INVITATION_START,
     CREATE_INVITATION_SUCCESS,
     CREATE_INVITATION_FAILURE,
-    UPDATE_INVITATION_START,
-    UPDATE_INVITATION_SUCCESS,
-    UPDATE_INVITATION_FAILURE,
     DELETE_INVITATION_START,
     DELETE_INVITATION_SUCCESS,
     DELETE_INVITATION_FAILURE,
@@ -20,11 +17,54 @@ import {
 } from '../constants/actionTypes';
 
 const initialState = {
-    
+    invitations: [],
+    isFetching: false,
+    currentInvitation: null,
+    error: null
 }
 
 export default (state = initialState, action) => {
     switch(action.type){
+        case GET_INVITATIONS_START:
+            return {
+                ...state,
+                isFetching: true
+            }
+        
+        case GET_INVITATIONS_SUCCESS:
+            return {
+                ...state,
+                invitations: action.payload,
+                isFetching: false
+            }
+
+        case GET_INVITATIONS_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+
+        case GET_SPECIFIC_INVITATION_START:
+            return {
+                ...state,
+                isFetching: true
+            }
+        
+        case GET_SPECIFIC_INVITATION_SUCCESS:
+            return {
+                ...state,
+                currentInvitation: action.payload,
+                isFetching: false
+            }
+
+        case GET_SPECIFIC_INVITATION_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+
         default:
             return state;
     }
