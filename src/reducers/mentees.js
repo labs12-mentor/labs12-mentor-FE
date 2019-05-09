076d5c5e82afb196ac2ea973a89689c8173a55c7
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-08 18:09:59 -0700

    updated mentees reducer - refactor

--- src/reducers/mentees.js
+++ src/reducers/mentees.js
@@ -30,37 +30,131 @@ export default (state = initialState, action) => {
     switch(action.type){
         case GET_MENTEES_START:
             return {
-                fetchingMentees: true
-            }
+                ...state,
+                isFetching: true
+            };
 
         case GET_MENTEES_SUCCESS:
             return {
-                fetchingMentees: false,
-                mentees: action.payload
-            }
+                ...state,
+                isFetching: false,
+                mentees: action.payload.sort((a, b) => {
+                    if (a.id < b.id) return -1;
+                    if (a.id > b.id) return 1;
+                    return 0;
+                })
+            };
 
         case GET_MENTEES_FAILURE:
             return {
-                fetchingMentees: false,
+                ...state,
+                isFetching: false,
                 error: action.payload
-            }
+            };
 
         case GET_SPECIFIC_MENTEE_START:
             return {
-                fetchingMentee: true
-            }
+                ...state,
+                isFetching: true
+            };
 
         case GET_SPECIFIC_MENTEE_SUCCESS:
             return {
-                fetchingMentee: false,
-                mentee: action.payload
-            }
+                ...state,
+                isFetching: false,
+                currentMentee: action.payload
+            };
 
         case GET_SPECIFIC_MENTEE_FAILURE:
             return {
-                fetchingMentee: false,
+                ...state,
+                isFetching: false,
                 error: action.payload
-            }
+            };
+        
+        case CREATE_MENTEE_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case CREATE_MENTEE_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                mentees: [...state.mentees, action.payload]
+            };
+
+        case CREATE_MENTEE_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
+        
+        case UPDATE_MENTEE_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case UPDATE_MENTEE_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                mentees: [...state.mentees.filter(elem => elem.id !== action.payload.id), action.payload].sort((a, b) => {
+                    if (a.id < b.id) return -1;
+                    if (a.id > b.id) return 1;
+                    return 0;
+                })
+            };
+
+        case UPDATE_MENTEE_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
+        
+        case DELETE_MENTEE_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case DELETE_MENTEE_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                mentees: state.mentees.filter(elem => elem.id !== action.payload)
+            };
+
+        case DELETE_MENTEE_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
+        
+        case REMOVE_MENTEE_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case REMOVE_MENTEE_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                mentees: state.mentees.filter(elem => elem.id !== action.payload)
+            };
+
+        case REMOVE_MENTEE_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
 
         default:
             return state;
