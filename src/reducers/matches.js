ccfe3e97d116f28fb287af69cc4d34d82c296cb3
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-08 17:50:29 -0700

    updated matches reducer - refactor

--- src/reducers/matches.js
+++ src/reducers/matches.js
@@ -20,11 +20,137 @@ import {
 } from '../constants/actionTypes';
 
 const initialState = {
-    
+    matches: [],
+    isFetching: false,
+    currentMatch: null,
+    error: null
 }
 
 export default (state = initialState, action) => {
     switch(action.type){
+        case GET_MATCHES_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case GET_MATCHES_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                matches: action.payload.sort((a, b) => {
+                    if (a.id < b.id) return -1;
+                    if (a.id > b.id) return 1;
+                    return 0;
+                })
+            };
+
+        case GET_MATCHES_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+
+        case GET_SPECIFIC_MATCH_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case GET_SPECIFIC_MATCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                currentMatch: action.payload
+            };
+
+        case GET_SPECIFIC_MATCH_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+
+        case CREATE_MATCH_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case CREATE_MATCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                matches: [...state.matches, action.payload]
+            };
+
+        case CREATE_MATCH_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+
+        case UPDATE_MATCH_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case UPDATE_MATCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                matches: [...state.matches.filter(elem => elem.id !== action.payload.id), action.payload].sort((a, b) => {
+                    if (a.id < b.id) return -1;
+                    if (a.id > b.id) return 1;
+                    return 0;
+                })
+            };
+
+        case UPDATE_MATCH_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+        
+        case DELETE_MATCH_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case DELETE_MATCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                matches: state.matches.filter(elem => elem.id !== action.payload)
+            };
+
+        case DELETE_MATCH_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+
+        case REMOVE_MATCH_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+
+        case REMOVE_MATCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                matches: state.matches.filter(elem => elem.id !== action.payload)
+            };
+
+        case REMOVE_MATCH_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+
+        
         default:
             return state;
     }
