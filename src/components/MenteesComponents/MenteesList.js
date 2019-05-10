eeb06875e4f379e718ea242e5058ddf981f89b9e
Jor_Poon <jormpoon@gmail.com>
2019-05-10 11:39:15 -0400

    wrote out code for MenteeList

--- src/components/MenteesComponents/MenteesList.js
+++ src/components/MenteesComponents/MenteesList.js
@@ -1,3 +1,44 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {getMentees} from '../../actions/mentees'
+import React from "react";
+import { connect } from "react-redux";
+import { getMentees } from "../../actions/mentees";
+
+class MenteesList extends React.Component {
+  state = {
+    isLoaded: false
+  };
+
+  async componentDidMount() {
+    await this.props.getMentees();
+    this.setState({ ...this.state, isLoaded: true });
+  }
+
+  render() {
+    const nonDeleted = this.props.mentees.filter(mentee => {
+      return mentee.deleted === false;
+    });
+
+    return (
+      <div>
+        <h1>Mentees</h1>
+        {this.state.isLoaded ? (
+          nonDeleted.map(mentee => {
+            return mentee.id;
+          })
+        ) : (
+          <h3>Loading</h3>
+        )}
+      </div>
+    );
+  }
+}
+
+const mstp = state => {
+  return {
+    mentees: state.mentees.mentees
+  };
+};
+
+export default connect(
+  mstp,
+  { getMentees }
+)(MenteesList);
