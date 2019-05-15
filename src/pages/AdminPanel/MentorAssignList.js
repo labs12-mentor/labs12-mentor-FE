25e95321f7ebabb441bcc37b8b45def03c06fe68
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-15 07:36:59 -0700

    added prop-types - mentor assign list

--- src/pages/AdminPanel/MentorAssignList.js
+++ src/pages/AdminPanel/MentorAssignList.js
@@ -1,18 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import history from '../../history';
-import {
-    Table,
-    ButtonGroup,
-    Button
-} from 'reactstrap';
+import { Table, ButtonGroup, Button } from 'reactstrap';
 
 class MentorAssignList extends React.Component {
-
     routeToAssignments(id) {
         history.push(`/user/admin/mentorassignment/${id}/mentor`);
     }
 
-    render() {        
+    render() {
         return (
             <Table striped>
                 <thead>
@@ -22,30 +18,34 @@ class MentorAssignList extends React.Component {
                         <th>City</th>
                         <th>Matched Student</th>
                         <th>Status</th>
-                        <th></th>
+                        <th />
                     </tr>
                 </thead>
 
                 <tbody>
-                    {this.props.matchedUsers.map(match => {
-                        return <tr key={match.id} onClick={() => this.routeToAssignments(match.id)}>
-                                   <td>{match.mentor.last_name}</td>
-                                   <td>{match.mentor.first_name}</td>
-                                   <td>{match.mentor.email}</td>
-                                   <td>{match.mentee.first_name + " " + match.mentee.last_name}</td>
-                                   <td>{match.status}</td>
-                                   <td>
-                                        <ButtonGroup>
-                                            <Button color="success">Approve</Button>
-                                            <Button color="danger">Deny</Button>
-                                        </ButtonGroup>
-                                   </td>
-                                </tr>
+                    {this.props.matchedUsers.map((match) => {
+                        return (
+                            <tr key={match.id} onClick={() => this.routeToAssignments(match.id)}>
+                                <td>{match.mentor.last_name}</td>
+                                <td>{match.mentor.first_name}</td>
+                                <td>{match.mentor.email}</td>
+                                <td>{match.mentee.first_name + ' ' + match.mentee.last_name}</td>
+                                <td>{match.status}</td>
+                                <td>
+                                    <ButtonGroup>
+                                        <Button color='success'>Approve</Button>
+                                        <Button color='danger'>Deny</Button>
+                                    </ButtonGroup>
+                                </td>
+                            </tr>
+                        );
                     })}
                 </tbody>
             </Table>
         );
     }
 }
+
+MentorAssignList.propTypes = {};
 
 export default MentorAssignList;
