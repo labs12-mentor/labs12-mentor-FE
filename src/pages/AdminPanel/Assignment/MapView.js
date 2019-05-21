3bc151e8e4689b40b71e2ba0bfa2cccd79d3c0be
jdspell <spelljd15@gmail.com>
2019-05-21 18:06:02 -0400

    added google maps to map view component

--- src/pages/AdminPanel/Assignment/MapView.js
+++ src/pages/AdminPanel/Assignment/MapView.js
@@ -2,36 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 
+import GoogleMaps from './GoogleMap';
+
 class MapView extends React.Component {
     render() {
         const mentor = this.props.mentorMatch;
         const mentee = this.props.menteeMatch;
         return (
-            <div className='MapView'>
-                {Object.keys(mentee).length === 0 ? (
-                    <div>
-                        <p>
-                            Address:{' '}
-                            {`${mentor.street}, ${mentor.city}, 
-                            ${mentor.state}, ${mentor.zipcode} ${mentor.country}`}
-                        </p>
+            <GoogleMaps />
+            // <div className='MapView'>
+            //     {Object.keys(mentee).length === 0 ? (
+            //         <div>
+            //             <p>
+            //                 Address:{' '}
+            //                 {`${mentor.street}, ${mentor.city}, 
+            //                 ${mentor.state}, ${mentor.zipcode} ${mentor.country}`}
+            //             </p>
 
-                        <p>Name: {`${mentor.first_name} ${mentor.last_name}`}</p>
-                        <Button>Assign</Button>
-                    </div>
-                ) : (
-                    <div>
-                        <p>
-                            Address:{' '}
-                            {`${mentee.street}, ${mentee.city}, 
-                            ${mentee.state}, ${mentee.zipcode} ${mentee.country}`}
-                        </p>
+            //             <p>Name: {`${mentor.first_name} ${mentor.last_name}`}</p>
+            //             <Button>Assign</Button>
+            //         </div>
+            //     ) : (
+            //         <div>
+            //             <p>
+            //                 Address:{' '}
+            //                 {`${mentee.street}, ${mentee.city}, 
+            //                 ${mentee.state}, ${mentee.zipcode} ${mentee.country}`}
+            //             </p>
 
-                        <p>Name: {`${mentee.first_name} ${mentee.last_name}`}</p>
-                        <Button>Assign</Button>
-                    </div>
-                )}
-            </div>
+            //             <p>Name: {`${mentee.first_name} ${mentee.last_name}`}</p>
+            //             <Button>Assign</Button>
+            //         </div>
+            //     )}
+            // </div>
         );
     }
 }
