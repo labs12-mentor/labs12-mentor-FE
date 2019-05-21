904b6955ebefc912b918621c224e8540c85af615
jdspell <spelljd15@gmail.com>
2019-05-20 21:51:15 -0400

    changed hero image on the login page

--- src/pages/AdminPanel/Application/Application.js
+++ src/pages/AdminPanel/Application/Application.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import NavPills from "../../../material-components/NavPills/NavPills.jsx";
 import { Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import classnames from 'classnames';
 
@@ -40,42 +41,26 @@ class Application extends React.Component {
     render() {
         return (
             <AppContainer>
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '1' })}
-                            onClick={() => {
-                                this.toggleTab('1');
-                            }}
-                        >
-                            Profile Information
-                        </NavLink>
-                    </NavItem>
-
-                    <NavItem>
-                        <NavLink
-                            className={classnames({ active: this.state.activeTab === '2' })}
-                            onClick={() => {
-                                this.toggleTab('2');
-                            }}
-                        >
-                            Application Response
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-
-                <TabContent activeTab={this.state.activeTab}>
-                    <TabPane tabId='1'>
-                        <ProfileInfo
-                            menteeId={this.props.match.params.id}
-                            currentUser={this.state.currentUser}
-                        />
-                    </TabPane>
-
-                    <TabPane tabId='2'>
-                        <ApplicationRes />
-                    </TabPane>
-                </TabContent>
+                <NavPills
+                    color="info"
+                    tabs={[
+                        {
+                        tabButton: "Profile Information",
+                        tabContent: (
+                            <ProfileInfo
+                                menteeId={this.props.match.params.id}
+                                currentUser={this.state.currentUser}
+                            />
+                        )
+                        },
+                        {
+                        tabButton: "Application Response",
+                        tabContent: (
+                            <ApplicationRes />
+                        )
+                        }
+                    ]}
+                />
             </AppContainer>
         );
     }
