f32c40c3d21d5d1ad19e6e1c235b662be7ac3f70
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-05-22 01:46:18 -0400

    added an action to send emails via contact form

---
+++ src/actions/contact.js
@@ -0,0 +1,48 @@
+import {
+    CONTACT_FORM_SEND_EMAIL_START,
+    CONTACT_FORM_SEND_EMAIL_SUCCESS,
+    CONTACT_FORM_SEND_EMAIL_FAILURE
+} from '../constants/actionTypes';
+import axios from 'axios';
+import { API_URL } from '../constants/config';
+import history from '../history';
+
+export function sendEmailViaContactForm(email) {
+    return async (dispatch) => {
+        await dispatch(request());
+
+        return await axios
+            .post(`${API_URL}/contact/sendemail`, email)
+            .then(async (res) => {
+                if (res.status === 200) {
+                    await dispatch(success(res.data));
+                    return await history.push('/');
+                } else {
+                    await dispatch(error(res.data.error));
+                    return await Promise.reject(res.data);
+                }
+            })
+            .catch(async (err) => {
+                return await dispatch(error(err));
+            });
+    };
+
+    function request() {
+        return {
+            type: CONTACT_FORM_SEND_EMAIL_START
+        };
+    }
+
+    function success(data) {
+        return {
+            type: CONTACT_FORM_SEND_EMAIL_SUCCESS
+        };
+    }
+
+    function error(err) {
+        return {
+            type: CONTACT_FORM_SEND_EMAIL_FAILURE,
+            payload: err
+        };
+    }
+}
